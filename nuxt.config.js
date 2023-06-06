3bbc671614c98b1e9936f3ebb521c61237e9f5fc
Olugbade Ayodeji <olugbade.ayodeji@gmail.com>
2023-06-06 15:41:39 +0100

    created an SCSS directory

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,8 +35,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: ['~assets/scss/_variable.scss'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
@@ -44,7 +43,6 @@ export default {
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
-
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/typescript
