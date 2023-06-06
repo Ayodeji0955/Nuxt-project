aceb705c54e62950a93e6bea42eabee3226d2f94
Olugbade Ayodeji <olugbade.ayodeji@gmail.com>
2023-06-06 02:03:14 +0100

    added nav components

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,22 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css' }
+    ],
+    script: [
+      {
+        src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js',
+        type: 'text/javascript'
+      },
+      {
+        src: 'https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js',
+        type: 'text/javascript'
+      },
+      {
+        src: '',
+        type: 'text/javascript'
+      }
     ]
   },
 
