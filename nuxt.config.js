b736cb906ac5b2ed0f1911c132a8ab7ede898097
Olugbade Ayodeji <olugbade.ayodeji@gmail.com>
2023-07-19 18:04:13 +0100

    added font-awesome cdn.js to the custom head and meta tags link in nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,7 +16,14 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css' }
+      { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css' },
+      {
+        rel: 'stylesheet',
+        href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+        integrity: 'sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==',
+        crossorigin: 'anonymous',
+        referrerpolicy: 'no-referrer'
+      }
     ],
     script: [
       {
@@ -40,27 +47,12 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
   ],
-
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build',
-    ['@nuxtjs/fontawesome', {
-      component: 'fa',
-      suffix: true,
-      proIcons: { // if you have bought the Pro packages
-        // list the icons you want to add, not listed icons will be tree-shaked
-        solid: [
-          'faHome',
-          'faHeart'
-        ],
-        // include all icons. But dont do this.
-        regular: true
-      }
-    }
-    ]
+    '@nuxt/typescript-build'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
